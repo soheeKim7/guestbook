17f8e4290382f520fe8c309132281f39c0bace67
soheeKim7 <okoksohee@gmail.com>
2023-04-20 15:50:04 +0900

    querydsl 사용법!!! 제목,내용 검색

--- guestbook/src/test/java/org/zerock/guestbook/repository/GuestBookRepositoryTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/repository/GuestBookRepositoryTests.java
@@ -1,15 +1,22 @@
 package org.zerock.guestbook.repository;
 
 import java.util.Optional;
+
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.zerock.guestbook.entity.GuestBook;
+import org.zerock.guestbook.entity.QGuestBook;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
 
 @SpringBootTest //test를 위해 꼭 필요!
 public class GuestBookRepositoryTests {
@@ -57,6 +64,30 @@ public class GuestBookRepositoryTests {
 		//PageRequest가 Pageable 인터페이스를 구현한거라서 페이저블로 바로 인터페이스로 받을 수 있음!!
 		Pageable pageable=  PageRequest.of(0, 100,Sort.by("gno").descending()); //10개씩 봤을때 첫페이지
 		//글번호로 내림차순 정렬해서 10개씩 봤을때 첫페이지
+		
+		////////////////////////////////////Querydsl 사용법//////////////////////////////////////////////////
+		//1. Querydsl 라이브러리 추가
+		//2. Querydsl 관련 빌드정보들 추가
+		//3. 빌드후에 QClass 이용해서 작업
+		//4. 추가로 Repository 인터페이스 설정시 Querydsl 관련 추가 상속
+		
+		
+		QGuestBook qGuestBook=QGuestBook.guestBook;
+		
+		//where 조건을 셋팅하기 위한 객체
+		BooleanBuilder builder = new BooleanBuilder();
+		
+		//원하는 where절 조건 셋팅
+		BooleanExpression expression=qGuestBook.title.contains("9");
+		BooleanExpression expression2=qGuestBook.content.contains("8");
+		
+		//where 조건 셋팅
+		builder.and(expression).and(expression2);
+		
+		//실행                                    //query 인터페이스를 통해서 추가된 메소드//오버로딩 됨!!
+		Page<GuestBook> list= guestBookRepository.findAll(builder,pageable);  //findAll 쓰기위해서 GuestBookRepository 인터페이스에 추가로 상속 받아야함!!!
+		
+		list.forEach(x->System.out.println(x));
 		
 	}
 	
